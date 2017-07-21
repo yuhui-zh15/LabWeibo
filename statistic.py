c46b6eb09d7775c372d82f4cb17941a9f4de9849
tcc <tcc@nlpnode04.(none)>
2017-07-22 00:39:55 +0800

    2

--- statistic.py
+++ statistic.py
@@ -8,20 +8,26 @@ import json
 import os
 import sys
 
-keywords = set()
+if len(sys.argv) != 2:
+	print 'Error!'
+	exit(-1)
+
+keywords = list()
 userdata = dict()
 with open('seedwords.txt') as fin:
 	line = fin.readline()
 	splitline = line.split(',')
 	for keyword in splitline:
-		keywords.add(keyword)
+		keywords.append(keyword)
 
-#filelist = ['microblogs0.txt', 'microblogs1.txt', 'microblogs2.txt', 'microblogs3.txt', 'microblogs4.txt', 'microblogs5.txt', 'microblogs6.txt', 'microblogs7.txt', 'microblogs8.txt', 'microblogs9.txt', 'microblogs10.txt', 'microblogs11.txt', 'microblogs12.txt', 'microblogs13.txt']
-filelist = ['dump1_microblogs_1KW_4.lzo']
+filelist = list()
+with open(sys.argv[1]) as fin:
+	for line in fin:
+		filelist.append(line.strip())
 srcpath = '/data/disk5/hadoop_backup/user/kuangchong/allweibodata/microblog/'
 dstpath = '/data/disk5/private/weibodata/'
 
-ferr = open('error.txt', 'w')
+ferr = open('error_' + sys.argv[1] + '.txt', 'w')
 for filename in filelist:
 	print('Unzip ' + filename + '...')
 	os.system('lzop -d ' + srcpath + filename + ' -p' + dstpath)
@@ -32,7 +38,7 @@ for filename in filelist:
 			try:
 				jsonline = json.loads(line)
 				uid = jsonline['user_id']
-				text = jsonline['text']
+				text = jsonline['text'].encode('utf-8')
 				if uid not in userdata:
 					userdata[uid] = [0, 0]
 				keywordsnum = 0
@@ -41,23 +47,19 @@ for filename in filelist:
 						keywordsnum += 1
 				userdata[uid][0] += 1
 				userdata[uid][1] += keywordsnum
-				print userdata[uid]
-				raw_input()
 			except:
 				ferr.write(line)	
-				pass
 	os.system('rm ' + dstpath + unzipfilename)
 ferr.close()
 
-fout = open('statistics.csv', 'w')
+fout = open('statistics_' + sys.argv[1] + '.csv', 'w')
 fout.write('uid,postnum,keywordsnum,ratio\n')
 for user in userdata.items():
 	uid = user[0]
 	postnum = user[1][0]
 	keywordsnum = user[1][1]
-	#ratio = 1.0 * keywordsnum / postnum
-	#fout.write(str(uid) + ',' + str(postnum) + ',' + str(keywordsnum) + ',' + str(ratio) + '\n')
-	fout.write(str(uid) + ',' + str(postnum) + ',' + str(keywordsnum) + '\n')
+	ratio = 1.0 * keywordsnum / postnum
+	fout.write(str(uid) + ',' + str(postnum) + ',' + str(keywordsnum) + ',' + str(ratio) + '\n')
 fout.close()
 
 		
