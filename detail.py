6e3afbb5f6d72e774098b18b29cba82508285736
tcc <tcc@nlpnode04.(none)>
2017-07-24 13:55:36 +0800

    3

--- detail.py
+++ detail.py
@@ -6,6 +6,7 @@
 
 import time
 import os
+import json
 
 userdata = dict()
 filelist = [
@@ -17,36 +18,31 @@ filelist = [
 'data6.txt'
 ]
 
-statistic_file = '/data/disk5/private/weibodata/filename/statistics_all.csv'
+statistic_file = '/data/disk5/private/weibodata/filelist/statistics_all.csv'
 srcpath = '/data/disk5/private/weibodata/user/'
 
 with open(statistic_file) as fin:
-	print fin.readline()
+	fin.readline()
 	for line in fin:
 		splitline = line.split(',')
 		uid = int(splitline[0])
 		postnum = int(splitline[1])
 		keywordsnum = int(splitline[2])
 		ratio = float(splitline[3])
-		userdata[uid] = [postnum, keywordsnum, ratio, 0, '']
+		userdata[uid] = [postnum, keywordsnum, ratio, 0, 'NOT_EXISTED']
 
-ferr = open('error_details.txt', 'w')
 for filename in filelist:
 	if (os.path.isfile(srcpath + filename) == False): continue
 	print('Now time = ' + time.asctime() + ', Processing ' + filename + '...')
 	with open(srcpath + filename) as fin:
 		for line in fin:
-			try:
-				jsonline = json.loads(line)
-				uid = jsonline['id']
-				name = jsonline['screen_name']
-				followerscount = jsonline['followers_count']
-				if uid not in userdata:
-					userdata[uid][3] = followerscount
-					userdata[uid][4] = name
-			except:
-				ferr.write(filename + '\t' + line)	
-ferr.close()
+			jsonline = json.loads(line.strip())
+			uid = jsonline['id']
+			name = jsonline['screen_name']
+			followerscount = jsonline['followers_count']
+			if uid in userdata:
+				userdata[uid][3] = followerscount
+				userdata[uid][4] = name
 
 fout = open('statistics_details.csv', 'w')
 fout.write('uid,postnum,keywordsnum,ratio,followerscount,name\n')
